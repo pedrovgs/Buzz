6a70d74b5737e4ede96d2fd115074d356d298aa9
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-03-10 18:13:47 +0100

    Let's force scale factor for testing purposes

--- src/testUtils/spectron.js
+++ src/testUtils/spectron.js
@@ -2,6 +2,7 @@ import { toMatchImageSnapshot } from "jest-image-snapshot";
 import { Application } from "spectron";
 import electron from "electron";
 import path from "path";
+import sharp from "sharp";
 
 expect.extend({ toMatchImageSnapshot });
 
@@ -12,13 +13,19 @@ let app;
 export async function compareScreenshot() {
   await waitForReady();
   const screenshot = await app.browserWindow.capturePage();
+  const size = await app.browserWindow.getSize();
+  console.log(size);
+  const width = size[0];
+  const height = size[1];
+  console.log(width);
+  console.log(height);
   expect(screenshot).toMatchImageSnapshot();
 }
 
 export async function startApp() {
   app = new Application({
     path: electron,
-    args: [path.join(__dirname, "..", "..", "electron-starter.js")],
+    args: [path.join(__dirname, "..", "..", "electron-starter.js"), ("--force-device-scale-factor=1")],
     startTimeout: 3000
   });
   return app.start();
