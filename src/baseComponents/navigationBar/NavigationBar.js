65cc37da7d515774412aa8d7c38375d1f4cace03
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-04-04 21:05:15 +0200

    Add the email to the navigation bar

--- src/baseComponents/navigationBar/NavigationBar.js
+++ src/baseComponents/navigationBar/NavigationBar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { AppBar, IconButton } from "material-ui";
+import { AppBar, FlatButton, IconButton } from "material-ui";
 import NavigationArrowBack from "material-ui/svg-icons/navigation/arrow-back";
 import { withRouter } from "react-router";
+import { connect } from "react-redux";
+
+const userEmailLabelStyle = {
+  textTransform: "lowercase"
+}
 
 class NavigationBar extends React.Component {
   constructor(props) {
@@ -16,7 +21,8 @@ class NavigationBar extends React.Component {
         title={this.props.title}
         onLeftIconButtonClick={this.onLeftIconButtonClick}
         iconElementLeft={this.leftIcon()}
-        showMenuIconButton={this.props.showBackButton | false}
+        showMenuIconButton={this.props.showBackButton || false}
+        iconElementRight={<FlatButton label={this.props.userLoggedInEmail} disabled={true} labelStyle={userEmailLabelStyle}/>}
       />
     );
   }
@@ -46,4 +52,14 @@ NavigationBar.propTypes = {
   history: PropTypes.object
 };
 
-export default withRouter(NavigationBar);
+const mapStateToProps = state => {
+  if (state.session.user) {
+    return {
+      userLoggedInEmail: state.session.user.email
+    };
+  } else {
+    return {};
+  }
+};
+
+export default withRouter(connect(mapStateToProps, () => {})(NavigationBar));
