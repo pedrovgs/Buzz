42cc98dc20a98cf9a2e54ed0aa7457af2150d8a8
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-04-09 22:53:56 +0200

    New feature added to be able to delete a picture

--- src/album/actions.js
+++ src/album/actions.js
@@ -1,10 +1,12 @@
 import { getPicturesByFolder } from "../firebase/firebaseStorage";
 import { getFolderId } from "../session/session";
 import testUtils from "../testUtils/utils";
+import { deletePictureFromFolder } from "../firebase/firebaseStorage";
 
 export const FETCH_PICTURES = "FETCH_PICTURES";
 export const PICTURES_LOADED = "PICTURES_LOADED";
 export const SELECT_PICTURE = "SELECT_PICTURE";
+export const DELETE_PICTURE = "DELETE_PICTURE";
 
 export function fetchPictures() {
   return function(dispatch, getState) {
@@ -30,5 +32,17 @@ export function selectPicture(pictureSelected) {
   return {
     type: SELECT_PICTURE,
     pictureSelected: pictureSelected
+  };
+}
+
+export function deletePicture(pictureToDelete) {
+  return function(dispatch, getState) {
+    const folderId = getFolderId(getState());
+    deletePictureFromFolder(folderId, pictureToDelete.id).then(() => {
+      dispatch({
+        type: DELETE_PICTURE,
+        pictureToDelete: pictureToDelete
+      });
+    });
   };
 }
--- src/album/album.js
+++ src/album/album.js
@@ -1,4 +1,9 @@
-import { FETCH_PICTURES, PICTURES_LOADED, SELECT_PICTURE } from "./actions";
+import {
+  DELETE_PICTURE,
+  FETCH_PICTURES,
+  PICTURES_LOADED,
+  SELECT_PICTURE
+} from "./actions";
 
 const initialState = {
   fetchingPictures: false,
@@ -20,7 +25,29 @@ export function albumReducer(state = initialState, action) {
       return Object.assign({}, state, {
         selectedPicture: action.pictureSelected
       });
+    case DELETE_PICTURE:
+      return Object.assign({}, state, {
+        selectedPicture: newSelectedPicture(
+          state.pictures,
+          action.pictureToDelete
+        ),
+        pictures: state.pictures.filter(
+          picture => picture.id !== action.pictureToDelete.id
+        )
+      });
     default:
       return state;
+  }
+}
+
+function newSelectedPicture(pictures, pictureToDelete) {
+  if (pictures.length === 1) {
+    return undefined;
+  } else {
+    const newSelectedPictureIndex = Math.max(
+      0,
+      pictures.indexOf(pictureToDelete) - 1
+    );
+    return pictures[newSelectedPictureIndex];
   }
 }
