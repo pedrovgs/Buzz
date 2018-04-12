1ab4fbbcf225196fee80a640f77e09673d15091c
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-04-13 00:05:23 +0200

    Update copy. The first version of the project but the pictures and gifs are ready now :smiley:

--- src/camera/CameraScreen.js
+++ src/camera/CameraScreen.js
@@ -35,7 +35,7 @@ class CameraScreen extends Component {
   render() {
     return fade(
       <div style={containerStyle}>
-        <NavigationBar title="&#128248; Cheeers!" showBackButton={true} />
+        <NavigationBar title="&#128248; Cheeese!" showBackButton={true} />
         <div>
           <WebCam ref={this.webcamReference} />
           <Countdown
